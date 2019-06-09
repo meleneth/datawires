cf4a4c39bb1f1b27128d0803e20db2a8d9de88ea
Meleneth <meleneth@rinzler>
2019-06-09 02:20:45 -0700

    Sketch in interface for adding new properties

--- src/views/SchemaEdit.vue
+++ src/views/SchemaEdit.vue
@@ -2,12 +2,27 @@
   div
     h3 {{ schema.dw$schema }}
     .md-layout
-      .md-layout-item
+      .md-layout-item.md-size-33
         pre(style="text-align: left")
           | {{ renderedJson }}
-      .md-layout-item
+      .md-layout-item.md-size-66
         | Hmm
         h5 Path: '{{ path }}'
+        .md-layout
+          .md-layout-item
+            md-field
+              label(for="new_property_type") Type
+              md-select(v-model="new_property_type" name="new_property_type" id="new_property_type")
+                md-option(value="string") string
+                md-option(value="object") object
+                md-option(value="array") array
+                md-option(value="number") number
+          .md-layout-item
+            md-field
+              label New Property Name
+              md-input(v-model="new_property_name")
+          .md-layout-item
+            md-button.md-raised.md-primary(v-on:click="add_property") Create
         .md-layout(v-for="property in properties" v-if="properties")
           .md-layout-item
             | {{ property.type }}
@@ -27,9 +42,13 @@ export default
     schema: false
     path: '/'
     properties: []
+    new_property_type: ''
+    new_property_name: ''
   mounted: ->
     @load_schema()
   methods:
+    add_property: ->
+      console.log "Adding property"
     load_schema: ->
       @$store.dispatch 'get', @id
         .then (schema) =>
