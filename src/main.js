94d59d45ed570d9be102a83343cf4f9cea82943f
Meleneth <meleneth@rinzler>
2019-06-08 19:37:47 -0700

    Initial mods so everything works

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+Vue.use(require('vue-moment'))
+
 
 Vue.config.productionTip = false
 
