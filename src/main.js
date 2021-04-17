f0092329d1bf93aea07cf9ce7d0dc6317a7103af
Meleneth <meleneth@rinzler>
2021-04-17 16:10:12 -0700

    initial vuetify removal and tailwind inclusion

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
 
 Vue.use(require('vue-moment'))
 
@@ -12,6 +11,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
