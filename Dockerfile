c714e0f446fc1bbbce11dc7daa8aa5f7ae854171
Meleneth <meleneth@rinzler>
2021-09-28 22:03:30 -0700

    Dockerise Datawires

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# build environment
+FROM node:14.17.6-bullseye as build
+WORKDIR /app
+RUN apt-get install python3
+COPY package.json package.json
+RUN yarn
+COPY . .
+RUN yarn build
+
+# production environment
+FROM nginx:latest
+COPY --from=build /app/dist /usr/share/nginx/html
+COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/components/DataView/DataInput.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+div
+  label
+  div
+    input
+component(:is="field_type" :field="field")
+  data-view(:field="f.field" v-for="f in fields")
+</template><script lang="coffee">
+import Vue from 'vue'
+
+import DataText from '@/components/DataView/DataText.vue'
+
+tlookup =
+  text: DataText
+  container: false
+  row: false
+  col: false
+  card: false
+  sheet: false
+  p: false
+  button: false
+
+DataView =
+  name: 'DataView'
+  components:
+    "data-view": false
+    "data-container": false
+  props:
+    field: Object
+  beforeCreate: ->
+    @$options.components['data-container'] = require('./DataContainer.vue').default
+    @$options.components['data-row'] = require('./DataRow.vue').default
+    @$options.components['data-col'] = require('./DataCol.vue').default
+    @$options.components['data-card'] = require('./DataCard.vue').default
+    @$options.components['data-grid'] = require('./DataGrid.vue').default
+    @$options.components['data-sheet'] = require('./DataSheet.vue').default
+    @$options.components['data-p'] = require('./DataP.vue').default
+    @$options.components['data-button'] = require('./DataButton.vue').default
+    tlookup['container'] = @$options.components['data-container']
+    tlookup['row'] = @$options.components['data-row']
+    tlookup['col'] = @$options.components['data-col']
+    tlookup['card'] = @$options.components['data-card']
+    tlookup['sheet'] = @$options.components['data-sheet']
+    tlookup['p'] = @$options.components['data-p']
+    tlookup['button'] = @$options.components['data-button']
+  computed:
+    field_type: ->
+      tlookup[@field.type]
+    fields: ->
+      if @field
+        return @field.children
+      return []
+
+DataView.components['data-view'] = DataView
+
+export default DataView
+</script>
