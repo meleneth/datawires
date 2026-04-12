65986ff7b5f6dcc23e1419ac5d8db07c050f7ed0
Meleneth <meleneth@gmail.com>
2026-04-12 02:25:40 -0700

    Big purge

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  def can?(_capability, **_context)
-    # TODO: integrate with real authorization system
-    true
-  end
-end
--- spec/factories/messages.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :message do
-    room { nil }
-    content { "MyText" }
-  end
-end
