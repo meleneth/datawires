d14209e07dc9e84f2f3dabdcad61e9c28d17f24e
meleneth <meleneth@gmail.com>
2026-06-21 08:05:01 +0000

    Prepare test database in devcontainer startup

--- .devcontainer/start-dev-server.sh
+++ .devcontainer/start-dev-server.sh
@@ -16,5 +16,6 @@ rm -f tmp/pids/server.pid tmp/pids/server.foreman.development.pid
 
 bundle check || bundle install
 bundle exec bin/rails db:prepare
+RAILS_ENV=test bundle exec bin/rails db:prepare
 
 exec bundle exec bin/rails server -u puma -p 3000 -b 0.0.0.0
