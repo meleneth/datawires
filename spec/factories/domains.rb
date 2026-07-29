4c7a57afde2e03d348b7546fe46d633f47ca5389
meleneth <meleneth@devastation.sectorfour>
2026-07-29 01:16:53 -0700

    Fix request spec domain ownership

--- spec/factories/domains.rb
+++ spec/factories/domains.rb
@@ -3,7 +3,11 @@
 FactoryBot.define do
   factory :domain do
     sequence(:name) { |n| "domain-#{n}" }
-    owner factory: :user
+    owner do
+      User.find_or_create_by!(id: ApplicationController::DEV_USER_ID) do |user|
+        user.name = "devUser"
+      end
+    end
     public { false }
   end
 end
