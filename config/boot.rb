8cb338ef1b0afc277ee4e0cfa012361b8388fc82
meleneth <meleneth@devastation.sectorfour>
2026-07-29 01:19:46 -0700

    Disable Bootsnap compile cache during coverage

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+ENV["DISABLE_BOOTSNAP_COMPILE_CACHE"] = "1" if defined?(Coverage) && Coverage.running?
 require "bootsnap/setup" unless ENV["DISABLE_BOOTSNAP"] # Speed up boot time by caching expensive operations.
